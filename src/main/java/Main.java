6691016c56936737c964c91c1b3aece490bfc23d
Anna Kochergina <akochergina92@gmail.com>
2023-10-05 20:11:37 +0300

    Stectrace

---
+++ src/main/java/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+        ProductRepository repo = new ProductRepository(); //создаем пустой репозиторий
+
+        try { //try говорим джаве что попытайся выполнить, если try выполнится без ошибок, то catch не будет выполнятся!
+            repo.removeById(-100);
+            //System.out.println("Hello!");
+        } catch (NegativeArraySizeException e) { //catch значит поймай, catch будет исполнятся если try завершиться ошибкой и программа пойдет исполняться дальше
+            System.out.println("ERROR");
+        }
+    }
+}
